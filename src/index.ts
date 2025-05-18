9e32bec1c1836f6de7c585ef659b1a4ac910bffb
Weston Gorczyca <weston.gorczyca@gmail.com>
2025-05-18 19:54:17 -0400

    fixed build error

--- src/index.ts
+++ src/index.ts
@@ -54,14 +54,14 @@ const commands = [
   }
 ];
 
-const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN || '');
 
 (async () => {
   try {
     console.log('Started refreshing application (/) commands.');
 
     await rest.put(
-      Routes.applicationCommands(process.env.CLIENT_ID),
+    Routes.applicationCommands(process.env.CLIENT_ID || ''),
       { body: commands },
     );
 
