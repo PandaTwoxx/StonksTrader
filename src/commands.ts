04d9f16a2a8c7ceb2ed4aea8956cffef69642de8
Weston Gorczyca <weston.gorczyca@gmail.com>
2025-06-07 13:09:13 -0400

    added hello to commands

--- src/commands.ts
+++ src/commands.ts
@@ -2,9 +2,12 @@ require('dotenv').config();
 const { REST, Routes, SlashCommandBuilder } = require('discord.js');
 
 const commands = [
-  new SlashCommandBuilder()
-    .setName('ping')
-    .setDescription('Replies with Pong!')
+    new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Replies with Pong!'),
+    new SlashCommandBuilder()
+        .setName('hello')
+        .setDescription('Replies with Hello!')
 ].map(cmd => cmd.toJSON());
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
